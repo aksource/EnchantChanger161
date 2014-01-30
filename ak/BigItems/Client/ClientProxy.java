2d7e8d91c4e8ed57ede02643746b8e576fedc4bc
aksource <akira.mew@gmail.com>
2014-01-30 22:46:24 +0900

    fix

--- ak/BigItems/Client/ClientProxy.java
+++ ak/BigItems/Client/ClientProxy.java
@@ -2,7 +2,6 @@ package ak.BigItems.Client;
 
 import net.minecraft.client.settings.KeyBinding;
 import net.minecraft.world.World;
-import net.minecraftforge.client.IItemRenderer;
 import net.minecraftforge.client.MinecraftForgeClient;
 
 import org.lwjgl.input.Keyboard;
@@ -10,9 +9,12 @@ import org.lwjgl.input.Keyboard;
 import ak.BigItems.BigItems;
 import ak.BigItems.CommonProxy;
 import cpw.mods.fml.client.FMLClientHandler;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
 
 public class ClientProxy extends CommonProxy
 {
+	@SideOnly(Side.CLIENT)
     public static KeyBinding MagicKey = new KeyBinding("Key.EcMagic",Keyboard.KEY_V);
 	@Override
 	public void registerClientInformation()
